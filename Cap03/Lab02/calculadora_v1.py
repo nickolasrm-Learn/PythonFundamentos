2e204c49b2fcac43cfbe4224d01ed553adcbe532
nickolasrm <nickolasrochamachado@gmail.com>
2021-10-09 22:02:27 -0300

    Calculadora laboratorio 2 feita
    * Usado dicionario para evitar ifs
    
    * Loop eterno
    
    * Adicionadas as operações +,-,*,/

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,24 @@
 
 print("\n******************* Python Calculator *******************")
 
+# Armazena as funções da calculadora
+# Evita ifs
+operacoes = {
+    '+': lambda x,y: x+y,
+    '-': lambda x,y: x-y,
+    '*': lambda x,y: x*y,
+    '/': lambda x,y: x/y
+}
 
+# Executa até o usuário escolher um operador que não existe
+while True
+    op = input(f'Escolha uma das operações a seguir {list(operacoes.keys())}: ')
+    # Se há a operação na calculadora, pergunte os numeros e faça a operação
+    if op in operacoes:
+        operando_esq = float(input('Primeiro numero: '))
+        operando_dir = float(input('Segundo numero: '))
+        print("**********RESULTADO**********")
+        print(operacoes.get(op)(operando_esq, operando_dir))
+    # Ou finalize o programa
+    else:
+        break
